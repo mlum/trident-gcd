d3c95e3dfb363cbdbb244661b62d52246765bbdd
Michael Lum <michael.lum@stampedemedia.net>
2015-03-03 01:01:49 -0800

    change target jvm

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject trident-gcd "0.0.1-wip2"
+(defproject trident-gcd "0.0.1-wip3"
   :description "This library implements a Trident state on top of the Google Cloud Datastore."
   :url "http://storm.apache.org/documentation/Trident-state.html"
   :license {:name "Apache License"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
-  :javac-options ["-Xlint:unchecked"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:unchecked"]
   :dependencies [
                  [com.google.apis/google-api-services-datastore-protobuf "v1beta2-rev1-2.1.0"]
                 ]
